0d6f776697f1151b0c4d7a6057121af9bf1dec92
Dang Quoc Huy <huysamdua@yahoo.com>
2018-07-18 12:47:15 +0700

    Step 2: Add acceptance/rejection

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.4.18;
 contract Escrow {
   address public buyer;
   address public seller;
-  uint256 public createdAt;
+  uint256 public createdAt = 0;
+  uint256 public buyerOK = 0;
+  uint256 public sellerOK = 0;
 
   constructor(address _seller) public payable {
     // TODO: validate deposit value???
@@ -11,5 +13,33 @@ contract Escrow {
     buyer = msg.sender;
     seller = _seller;
     createdAt = now;
+  }
+
+  function accept() public {
+    if (msg.sender == buyer && buyerOK == 0) {
+      buyerOK = now;
+    } else if (msg.sender == seller && sellerOK == 0) {
+      sellerOK = now;
+    }
+    // if both OK, release money
+    if (buyerOK != 0 && sellerOK != 0) {
+      // release money to seller
+      seller.transfer(address(this).balance);
+      // then, destroy contract
+      selfdestruct(buyer);
+    }
+  }
+
+  function reject() public {
+    if (msg.sender == seller) {
+      // refund to buyer
+      buyer.transfer(address(this).balance);
+    } else if (msg.sender == buyer) {
+      // TODO: make sure seller get their goods back
+      // then, refund to buyer
+      buyer.transfer(address(this).balance);
+    }
+    // destroy contract
+    selfdestruct(buyer);
   }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,5 @@
+///// Backup auto-code /////
+/*
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
 
@@ -5,4 +7,11 @@ module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+};
+*/
+
+var Escrow = artifacts.require("Escrow.sol");
+
+module.exports = function(deployer, network, accounts) {
+  deployer.deploy(Escrow, accounts[1], {from: accounts[0], value: web3.toWei(1, "finney")});
 };
